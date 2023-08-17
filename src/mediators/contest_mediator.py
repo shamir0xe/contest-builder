07a0e8fe9d1328740da37028b2231f841cae0228
shamir0xe <shamir0xe@gmail.com>
2023-08-17 17:30:55 +0330

    refactor: pass whole defaults.json file as an DTO to contest variable

--- src/mediators/contest_mediator.py
+++ src/mediators/contest_mediator.py
@@ -1,30 +1,26 @@
 from __future__ import annotations
 from libs.pylib.config.config import Config
 from libs.pylib.argument.argument_parser import ArgumentParser
+from libs.pylib.data.data_transfer_object import DataTransferObject
 from src.generators.contest_generator import ContestGenerator
 from src.actions.name_type_mapper import name_type_mapper
-from libs.pylib.data.data_transfer_object import DataTransferObject
 
 
 class ContestMediator:
     def read_configs(self) -> ContestMediator:
         self.attributes = Config.read("defaults").keys()
-        self.contest = DataTransferObject()
-        self.contest.site = Config.read("defaults.site")
-        self.contest.name = Config.read("defaults.name", default=None)
-        self.contest.name_type = Config.read("defaults.name_type")
-        self.contest.problem_cnt = Config.read("defaults.problem_cnt")
-        self.contest.template_path = Config.read("defaults.template_path")
+        self.contest = DataTransferObject.from_dict(Config.read('defaults'))
         return self
 
     def read_args(self) -> ContestMediator:
         for key in ArgumentParser.get_options():
             key = key.lower()
             value = ArgumentParser.get_value(key)
+            # help option resolver
             if key == 'help' or not hasattr(self.contest, key):
                 print(f'available options are: {list(self.attributes)}')
                 exit(0)
-            if not hasattr(self.contest, key) or value is None:
+            if value is None:
                 continue
             setattr(self.contest, key, value[0])
         if getattr(self.contest, "name") is None:
