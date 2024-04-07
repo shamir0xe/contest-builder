443d0f14d499262d0f69375cd5ec781d29e306a5
shamir0xe <shamir0xe@gmail.com>
2024-04-08 01:29:38 +0330

    refactor: replace pylib_0xe instead of local lib, add docs

--- src/models/provider.py
+++ src/models/provider.py
@@ -1,15 +1,24 @@
-from __future__ import annotations
 from dataclasses import dataclass, field
-from libs.pylib.data.data_transfer_object import DataTransferObject
+from pylib_0xe.data.data_transfer_object import DataTransferObject
 
 
 @dataclass
 class Provider(DataTransferObject):
+    """
+    Represents a provider.
+    """
+
     name: str = ""
     abbreviations: list[str] = field(default_factory=list)
 
     def name_mapper(self, name: str) -> str:
+        """
+        Mapper function for provider name.
+        """
         return name.lower()
 
     def abbreviations_mapper(self, abbr_list: list[str]) -> list[str]:
-        return list(map(lambda abbr: abbr.lower(), abbr_list))
+        """
+        Mapper function for provider abbreviations.
+        """
+        return [abbr.lower() for abbr in abbr_list]
