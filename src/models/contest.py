cd6f580d12286759cfff9f085c6c811daf7df44b
shamir0xe <shamir0xe@gmail.com>
2024-04-08 01:20:49 +0330

    docs: add docs to functions of contest model

--- src/models/contest.py
+++ src/models/contest.py
@@ -5,11 +5,15 @@ from src.helpers.model.problem_helper import ProblemHelper
 from src.models.language import Language
 from src.models.provider import Provider
 from src.types.name_types import NameTypes
-from libs.pylib.data.data_transfer_object import DataTransferObject
+from pylib_0xe.data.data_transfer_object import DataTransferObject
 
 
 @dataclass
 class Contest(DataTransferObject):
+    """
+    Represents a contest.
+    """
+
     name_type: NameTypes = NameTypes.ALPHABETICAL
     name: str = ""
     problem_cnt: int = 0
@@ -19,21 +23,37 @@ class Contest(DataTransferObject):
 
     @staticmethod
     def modifiables() -> list[str]:
+        """
+        Returns a list of modifiable attributes.
+        """
         return ["name", "provider", "language", "problem_cnt", "name_type"]
 
     def problem_cnt_mapper(self, number: int | str) -> int:
+        """
+        Mapper function for problem count.
+        """
         return int(number)
 
     def name_type_mapper(self, key: str) -> NameTypes:
+        """
+        Mapper function for name types.
+        """
         return ProblemHelper.name_type_mapper(key)
 
     def name_mapper(self, name: str) -> str:
+        """
+        Mapper function for contest name.
+        """
         return ProblemHelper.problem_name_mapper(name)
 
     def provider_mapper(self, provider: str) -> Provider:
-        # set provider name
+        """
+        Mapper function for contest provider.
+        """
         return ProviderFinder.by_abbreviation(provider)
 
     def language_mapper(self, language: str) -> Language:
-        # set the language
+        """
+        Mapper function for contest language.
+        """
         return LanguageFinder.by_abbreviation(language)
