0cbda0d5994334750273466211d97b15e5b74670
shamir0xe <shamir0xe@gmail.com>
2024-04-08 02:20:51 +0330

    refactor: change main script behavior to option-handler structure

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
+from functools import partial
 from src.mediators.contest_mediator import ContestMediator
 from src.mediators.execution_mediator import ExecutionMediator
 from src.mediators.problem_mediator import ProblemMediator
 from src.mediators.init_mediator import InitMediator
-from libs.pylib.argument.argument_parser import ArgumentParser
+from pylib_0xe.argument.argument_parser import ArgumentParser
 
 ARG_PREFIX = "--"
 
+# Mapping command-line options to handler functions
+option_handlers = {
+    "init": partial(
+        InitMediator().read_configs().create_local_config().create_templates().closure
+    ),
+    "run": partial(
+        ExecutionMediator()
+        .read_configs()
+        .read_args()
+        .extract_language()
+        .extract_name()
+        .execute
+    ),
+    "problem": partial(ProblemMediator().read_configs().read_args().generate().closure),
+    "contest": partial(ContestMediator().read_configs().read_args().generate().closure),
+}
+
+
+def main():
+    """Main function."""
+    option = ArgumentParser.get_option(option_prefix=ARG_PREFIX)
+    if option in option_handlers:
+        option_handlers[option]()
+    else:
+        print("Invalid option")
+
 
 if __name__ == "__main__":
-    if ArgumentParser.is_option("init", option_prefix=ARG_PREFIX):
-        InitMediator().read_configs().create_local_config().create_templates().closure()
-    elif ArgumentParser.is_option("run", option_prefix=ARG_PREFIX):
-        # calling program mediator
-        ExecutionMediator().read_configs().read_args().extract_language().extract_name().execute()
-    elif ArgumentParser.is_option("problem", option_prefix=ARG_PREFIX):
-        # calling problem mediator
-        ProblemMediator().read_configs().read_args().generate().closure()
-    else:
-        # calling contest mediator
-        ContestMediator().read_configs().read_args().generate().closure()
+    main()
