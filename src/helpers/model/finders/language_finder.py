57afd6cb20d2fe161baae962a1f9446580e25e8c
shamir0xe <shamir0xe@gmail.com>
2024-04-08 15:36:09 +0330

    refactor: change language finder based on SRP

--- src/helpers/model/finders/language_finder.py
+++ src/helpers/model/finders/language_finder.py
@@ -5,27 +5,37 @@ from src.models.language import Language
 class LanguageFinder:
     @staticmethod
     def by_name(name: str) -> Language:
-        return list(
-            filter(lambda language: name == language.name, LanguageFinder.all())
-        )[0]
+        filtered = LanguageFinder.filter_languages(
+            lambda language: name == language.name
+        )
+        return LanguageFinder.get_single_language(filtered, name)
 
     @staticmethod
     def by_abbreviation(abbreviation: str) -> Language:
-        return list(
-            filter(
-                lambda language: abbreviation in language.abbreviations,
-                LanguageFinder.all(),
-            )
-        )[0]
+        filtered = LanguageFinder.filter_languages(
+            lambda language: abbreviation in language.abbreviations
+        )
+        return LanguageFinder.get_single_language(filtered, abbreviation)
 
     @staticmethod
     def all() -> list[Language]:
-        res = []
         languages = LocalConfig.read("languages")
-        for language, properties in languages.items():
-            res.append(Language(name=language).from_dict(properties))
-        return res
+        return [
+            Language.from_dict({"name": language, **properties})
+            for language, properties in languages.items()
+        ]
 
     @staticmethod
     def default() -> Language:
-        return LanguageFinder.by_name(LocalConfig.read("problem.language"))
+        problem_language = LocalConfig.read("problem.language")
+        return LanguageFinder.by_name(problem_language)
+
+    @staticmethod
+    def filter_languages(predicate) -> list[Language]:
+        return list(filter(predicate, LanguageFinder.all()))
+
+    @staticmethod
+    def get_single_language(filtered: list[Language], search_criteria: str) -> Language:
+        if not filtered:
+            raise Exception(f"No valid language found by {search_criteria}")
+        return filtered[0]
