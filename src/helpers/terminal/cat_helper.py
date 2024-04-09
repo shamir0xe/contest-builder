9d31a29e2adae08408f127b5a9d3e8aa44e3adfc
shamir0xe <shamir0xe@gmail.com>
2024-04-09 17:04:06 +0330

    feat: add CatHelper to handle script parameters

---
+++ src/helpers/terminal/cat_helper.py
@@ -0,0 +1,14 @@
+from src.helpers.terminal.arg_helper import ArgHelper
+
+
+class CatHelper:
+    @staticmethod
+    def meow():
+        helper = (
+            ArgHelper(["name", "problem", "run", "init"])
+            .check_help()
+            .check_help("elp")
+            .check_args()
+        )
+        if len(helper.get_data()) == 0:
+            helper.show_valid_options().done()
