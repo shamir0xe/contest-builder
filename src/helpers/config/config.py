7743bc103b819dbf59215ce43e8b518d932a3f76
shamir0xe <shamir0xe@gmail.com>
2025-02-03 18:44:29 +0330

    refactor: Make compatible with the newer version of pylib_0xe

--- src/helpers/config/config.py
+++ src/helpers/config/config.py
@@ -2,8 +2,7 @@ from pylib_0xe.config.config import Config as CFG
 from typing import Any
 
 
-class Config():
+class Config:
     @staticmethod
     def read(selector: str, **kwargs) -> Any:
-        config = CFG(file_path=__file__)
-        return config.read(selector, **kwargs)
+        return CFG.read(selector, base_path=__file__)
